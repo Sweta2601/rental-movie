8634b25c4e7689da8a072a7d12a0031cd8fb68c1
Sweta2601 <kumarisweta1309@gmail.com>
2026-01-12 20:53:17 +0530

    Remove config.py from repository

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    # Format: mysql+pymysql://username:password@host/database_name
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql+pymysql://appuser:app123@localhost/movie_rental_db?local_infile=1'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
