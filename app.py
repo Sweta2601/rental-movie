bfba2ebbc0e3286a56c84e3c55853fa7cedc8767
Sweta2601 <kumarisweta1309@gmail.com>
2026-01-13 19:04:34 +0530

    Finalize Flask app initialization

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask
+from config import Config
+from database.db import db
 
+from routes.user_routes import user_bp
+from routes.provider_routes import provider_bp
+from routes.movie_routes import movie_bp
+from routes.rating_routes import rating_bp
+from routes.rental_routes import rental_bp
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
+
+    if testing:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        app.config["TESTING"] = True
+    else:
+        app.config.from_object("config.Config")
 
     @app.route("/")
     def hello_world():
         return "app is running!"
-    
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    app.register_blueprint(user_bp)
+    app.register_blueprint(provider_bp)
+    app.register_blueprint(movie_bp)
+    app.register_blueprint(rating_bp)
+    app.register_blueprint(rental_bp, url_prefix="/rentals")
+
+
+    with app.app_context():
+        db.create_all()
+
     return app
 
 app = create_app()
